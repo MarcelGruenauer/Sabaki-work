d18cd27cb664a57b63d7f593802f35e00e5662f2
Marcel Gruenauer <github.attic184@passmail.net>
2026-06-11 16:22:16 +0200

    Use thumbnail markers for game previews

--- src/components/drawers/GameChooserDrawer.js
+++ src/components/drawers/GameChooserDrawer.js
@@ -20,6 +20,12 @@ let itemMinWidth = thumbnailSize + 12 + 20
 let itemHeight = 253 + 10 + 20
 
 let getPreviewBoard = (tree) => {
+  for (let node of tree.listNodes()) {
+    if (node.data.N != null && node.data.N.includes('thumbnail')) {
+      return gametree.getBoard(tree, node.id)
+    }
+  }
+
   let node = tree.navigate(tree.root.id, 30, {})
   if (!node)
     node = tree.navigate(tree.root.id, tree.getCurrentHeight({}) - 1, {})
